7023fb2c9f196966401d1e94483940d39187ad3a
SHerlihy <65720475+SHerlihy@users.noreply.github.com>
2021-02-22 18:15:37 +0000

    setup commit

---
+++ storefront/src/Components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="nav">
+      <nav className="nav__brand">eSTORE</nav>
+      <div className="nav__btns">
+        <nav>SIGN UP</nav>
+        <nav>CART</nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
